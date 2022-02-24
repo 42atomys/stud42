455dcf98326962417af52fa150edbfe19200eef3
Gregory DALMAR <contact@atomys.fr>
2022-02-24 02:09:21 +0100

    feat: Use subprocess spawn instead static to support all features of nextJS (https://nextjs.org/docs/advanced-features/static-html-export\#unsupported-features)

--- cmd/interface.go
+++ cmd/interface.go
@@ -5,14 +5,16 @@ Copyright © 2022 42Atomys
 package cmd
 
 import (
-	"net/http"
+	"bufio"
+	"fmt"
+	"os/exec"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
 var (
-	interfacePortFlag *string
+	interfacePortFlag, interfaceNextPath *string
 )
 
 // interfaceCmd represents the interface command
@@ -22,11 +24,35 @@ var interfaceCmd = &cobra.Command{
 
 	Run: func(cmd *cobra.Command, args []string) {
 		// The static Next.js app will be served under `/`.
-		http.Handle("/", http.FileServer(http.Dir("web/ui/dist")))
+		yarn := exec.Command(*interfaceNextPath, "start", "web/ui", "--port", *interfacePortFlag)
 
-		// Start HTTP server at given port or default 3000 port
-		log.Info().Msgf("Serving NextJS interface on port %s", *interfacePortFlag)
-		log.Fatal().Err(http.ListenAndServe(":"+*interfacePortFlag, nil)).Msg("Error serving NextJS interface")
+		stdOut, err := yarn.StdoutPipe()
+		if err != nil {
+			log.Fatal().Err(err).Msg("Error creating StdoutPipe for Cmd")
+		}
+		defer stdOut.Close()
+
+		scanner := bufio.NewScanner(stdOut)
+		go func() {
+			for scanner.Scan() {
+				log.Info().Str("process", "interface").Msg(scanner.Text())
+			}
+		}()
+
+		err = yarn.Start()
+		if err != nil {
+			log.Fatal().Err(err).Msg("Error during the start of interface")
+		}
+
+		log.Info().Str("process", "interface").Msgf("Serving NextJS interface on port %s", *interfacePortFlag)
+		err = yarn.Wait()
+		// go generate command will fail when no generate command find.
+		if err != nil {
+			if err.Error() != "exit status 1" {
+				fmt.Println(err)
+				log.Fatal().Err(err).Msg("Error during the execution of interface")
+			}
+		}
 	},
 }
 
@@ -34,4 +60,5 @@ func init() {
 	serveCmd.AddCommand(interfaceCmd)
 
 	interfacePortFlag = interfaceCmd.Flags().StringP("port", "p", "3000", "port used to serve the interface")
+	interfaceNextPath = interfaceCmd.Flags().StringP("nextPath", "n", "web/ui/node_modules/next/dist/bin/next", "NextJS executable path")
 }
