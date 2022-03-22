cd7034dfeca66beebbd6f7fb5424a214b49392aa
Atomys <contact@atomys.fr>
2022-03-22 21:03:24 +0100

    test: Add Tooltip test unit

--- web/ui/src/components/Tooltip/Tooltip.tsx
+++ web/ui/src/components/Tooltip/Tooltip.tsx
@@ -15,12 +15,14 @@ export const Tooltip = ({
       className="relative flex items-center"
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
+      data-testid="hover-target"
     >
       <div
         className={classNames(
           visible ? 'opacity-100 z-10 ml-4' : '-z-10 opacity-0 ml-1',
           'hidden md:flex left-full absolute whitespace-no-wrap bg-gradient-to-r from-indigo-600 to-indigo-500 text-slate-100 dark:text-slate-300 px-4 py-2 rounded items-center transition-all duration-150'
         )}
+        data-testid="tooltip"
       >
         <div
           className="bg-indigo-600 h-3 w-3 absolute"
--- web/ui/src/components/Tooltip/__tests__/snapshot.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import Tooltip from '@components/Tooltip'
-
-it('renders homepage unchanged', () => {
-  const { container } = render(<Tooltip text="tooltip-test">Test</Tooltip>)
-  expect(container).toMatchSnapshot()
-})
