9c2783cfd75dde96d76acaf4da3ca05e013198c2
Gregory DALMAR <contact@atomys.fr>
2022-04-07 23:00:49 +0200

    feat: Add country flag on Location Badge

--- web/ui/src/components/Badge/LocationBadge.tsx
+++ web/ui/src/components/Badge/LocationBadge.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import Twemoji from "react-twemoji";
 import { Location } from "types/globals";
 import { Badge } from "./Badge";
 
@@ -7,7 +8,11 @@ export const LocationBadge = (location: Location) => {
   const isConnected = location.host ? true : false;
 
   return <Badge color={isConnected ? 'green' : 'gray'}>
-    <span className={classNames('inline-flex rounded-full w-2 h-2 mr-1', isConnected ? 'bg-emerald-500' : 'bg-slate-500')}></span>
-    <span className='text-sm'>{isConnected ? location.host : 'Offline'}</span>
+    <Twemoji noWrapper={true} options={{ folder: 'svg', ext: '.svg', className: 'mx-1 w-[14px] h-[14px]' }}>
+      <span className={classNames('inline-flex rounded-full w-2 h-2 mr-1', isConnected ? 'bg-emerald-500' : 'bg-slate-500')}></span>
+      <span className='flex flex-row justify-center items-center text-sm'>{isConnected ? location.host : 'Offline'}</span>
+      <span className={isConnected ? 'visible' : 'hidden'}>🇫🇷</span>
+    </Twemoji>
+    <span></span>
   </Badge>
 }
