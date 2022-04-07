0d0a60d6e5e3830192531382bbc99297e652b707
42Atomys <contact@atomys.fr>
2022-04-07 23:06:01 +0200

    feat: Add the login page design and next-auth configuration (#47)

--- web/ui/next.config.js
+++ web/ui/next.config.js
@@ -14,6 +14,29 @@ const nextConfig = {
       exclude: /node_modules/,
       loader: 'graphql-tag/loader',
     });
+
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg')
+    )
+    fileLoaderRule.exclude = /\.svg$/
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.tsx?$/,
+      use: [
+        'next-swc-loader',
+        {
+          loader: '@svgr/webpack',
+          options: { babel: false }
+        }
+      ],
+    });
+
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
     return config;
   },
   webpackDevMiddleware: (config) => {
