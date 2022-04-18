c8f2a36217608f87333b88545610265a5bc52ba0
42Atomys <contact@atomys.fr>
2022-04-18 08:51:24 +0200

    feat: Implement the JWTKS Service (#65)
    * feat: Implement the JWTKS Service
    
    * docs: Add doc to start JWTKS Service without DEVENV
    
    * feat: Secure the GRPC server with certificates
    
    * fix: Install protoc compiler dependencies on CI
    
    * fix: Log of JWTKS certificates cause tests crashes

---
+++ cmd/jwtks.go
@@ -0,0 +1,59 @@
+/*
+Copyright © 2022 42Atomys
+*/
+package cmd
+
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+//go:generate protoc -I ../api/grpc --go_out=../ --go-grpc_out=../ ../api/grpc/jwtks.proto
+
+import (
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"atomys.codes/stud42/internal/config"
+	"atomys.codes/stud42/internal/jwtks"
+	_ "atomys.codes/stud42/internal/models/generated/runtime"
+)
+
+var (
+	jwtksGrpcPortFlag *string
+	jwtksHttpPortFlag *string
+)
+
+// jwtksCmd represents the jwtks command
+var jwtksCmd = &cobra.Command{
+	Use:   "jwtks",
+	Short: "Serve the ID Provider service",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := config.Load(); err != nil {
+			log.Fatal().Err(err).Msg("invalid configuration")
+		}
+
+		if err := jwtks.SetCertificates(viper.GetString("jwtks.jwk.cert_private_key"), viper.GetString("jwtks.jwk.cert_public_key")); err != nil {
+			log.Fatal().Err(err).Msg("failed to set jwtks keys")
+		}
+
+		var errChan = make(chan error)
+
+		go func() {
+			errChan <- jwtks.ServeGRPC(jwtksGrpcPortFlag)
+		}()
+
+		go func() {
+			errChan <- jwtks.ServeHTTP(jwtksHttpPortFlag)
+		}()
+
+		log.Fatal().Err(<-errChan).Msg("failed to serve")
+	},
+}
+
+func init() {
+	serveCmd.AddCommand(jwtksCmd)
+
+	jwtksGrpcPortFlag = jwtksCmd.Flags().String("gport", "5000", "port used to serve the grpc server of the jwt-provider")
+	jwtksHttpPortFlag = jwtksCmd.Flags().String("hport", "5500", "port used to serve the http server of the jwt-provider")
+}
