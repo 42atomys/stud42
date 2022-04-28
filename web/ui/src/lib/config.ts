42e4dda8ed59c19f0d1a941603b15575b198d86c
Gregory DALMAR <contact@atomys.fr>
2022-04-28 20:34:31 +0200

    feat: Deploy the interface on the next app

--- web/ui/src/lib/config.ts
+++ web/ui/src/lib/config.ts
@@ -3,6 +3,9 @@ import YAML from "yaml"
 
 
 export const getConfig = (): Configuration => {
-  const fileContent = readFileSync("../../stud42.config.yaml", "utf8")
+  if (!process.env.CONFIG_PATH)
+    throw new Error("No CONFIG_PATH env var set.")
+
+  const fileContent = readFileSync(process.env.CONFIG_PATH, "utf8")
   return YAML.parse(fileContent) as Configuration
 }
