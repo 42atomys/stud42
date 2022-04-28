8ca743f9acf98fd0a3257d1634f3d510f20cbc7d
Gregory DALMAR <contact@atomys.fr>
2022-04-29 00:00:53 +0200

    chore: Use camelCase for yaml configuration file

--- internal/jwtks/grpc.go
+++ internal/jwtks/grpc.go
@@ -45,8 +45,8 @@ func ServeGRPC(port *string) error {
 		s = grpc.NewServer()
 	} else {
 		creds, err := credentials.NewServerTLSFromFile(
-			viper.GetString("jwtks.grpc.cert_public_key_file"),
-			viper.GetString("jwtks.grpc.cert_private_key_file"),
+			viper.GetString("jwtks.grpc.certPublicKeyFile"),
+			viper.GetString("jwtks.grpc.certPrivateKeyFile"),
 		)
 		if err != nil {
 			log.Fatal().Msgf("failed to load credentials: %v", err)
