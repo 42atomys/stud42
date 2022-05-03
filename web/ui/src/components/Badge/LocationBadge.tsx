8df662622815fc1d5dda4a1c6e2d633a720e6b8c
Atomys <contact@atomys.fr>
2022-05-04 01:20:00 +0200

    clean: Remove old Tweemoji dependencies

--- web/ui/src/components/Badge/LocationBadge.tsx
+++ web/ui/src/components/Badge/LocationBadge.tsx
@@ -1,5 +1,5 @@
+import Emoji from "@components/Emoji";
 import classNames from "classnames";
-import Twemoji from "react-twemoji";
 import { Location } from "types/globals";
 import { Badge } from "./Badge";
 
@@ -8,11 +8,8 @@ export const LocationBadge = (location: Location) => {
   const isConnected = location.host ? true : false;
 
   return <Badge color={isConnected ? 'green' : 'gray'}>
-    <Twemoji noWrapper={true} options={{ folder: 'svg', ext: '.svg', className: 'mx-1 w-[14px] h-[14px]' }}>
-      <span className={classNames('inline-flex rounded-full w-2 h-2 mr-1', isConnected ? 'bg-emerald-500' : 'bg-slate-500')}></span>
-      <span className='flex flex-row justify-center items-center text-sm'>{isConnected ? location.host : 'Offline'}</span>
-      <span className={isConnected ? 'visible' : 'hidden'}>🇫🇷</span>
-    </Twemoji>
-    <span></span>
+    <span className={classNames('inline-flex rounded-full w-2 h-2 mr-1', isConnected ? 'bg-emerald-500' : 'bg-slate-500')}></span>
+    <span className='flex flex-row justify-center items-center text-sm'>{isConnected ? location.host : 'Offline'}</span>
+    <Emoji emoji="🇫🇷" size={14} className={classNames('mx-1', isConnected ? 'visible' : 'hidden')} />
   </Badge>
 }
