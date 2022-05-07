cc099b55b67bc561451850acb6d3fc38a6d40a33
Atomys <contact@atomys.fr>
2022-05-07 23:06:26 +0200

    gs

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 OS=$([ "$(uname -s)" == 'Darwin' ] && echo "osx" || echo "$(uname -s)")
-curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | \
+URL=$(curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | \
   jq -c ".assets | map(select( .name | contains(\"protoc\") and contains(\"$(uname -m)\") and contains(\"$OS\"))) | .[].browser_download_url" | \
-  tr -d \" | \
-  xargs wget -O /tmp/protoc.zip
+  tr -d \")
+
+echo "Downloading protoc... $URL"
+wget -qO /tmp/protoc.zip $URL
 unzip -uo /tmp/protoc -d /usr/local bin/protoc
