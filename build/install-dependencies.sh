f57a3bdc104b598b7079f852a09c3054a3991110
Atomys <contact@atomys.fr>
2022-05-07 23:11:08 +0200

    fix: Fallback to linux when no platform is found

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -5,6 +5,13 @@ URL=$(curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/lat
   jq -c ".assets | map(select( .name | contains(\"protoc\") and contains(\"$(uname -m)\") and contains(\"$OS\"))) | .[].browser_download_url" | \
   tr -d \")
 
+if [ -z "$URL" ]; then
+  echo "Could not find protoc binary for your platform. Fallback to linux"
+  URL=$(curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | \
+    jq -c ".assets | map(select( .name | contains(\"protoc\") and contains(\"x86_64\") and contains(\"linux\"))) | .[].browser_download_url" | \
+    tr -d \") 
+fi
+
 echo "Downloading protoc... $URL"
 wget -qO /tmp/protoc.zip $URL
 unzip -uo /tmp/protoc -d /usr/local bin/protoc
