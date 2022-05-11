5407a81622a895028517448dd954e58efcf2103c
Atomys <contact@atomys.fr>
2022-05-11 16:43:11 +0200

    fix: Campus duplication when rennung the job again

--- cmd/campus.go
+++ cmd/campus.go
@@ -22,14 +22,11 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"context"
-	"database/sql"
-	"errors"
-
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
 	modelsutils "atomys.codes/stud42/internal/models"
+	modelgen "atomys.codes/stud42/internal/models/generated"
 	"atomys.codes/stud42/pkg/duoapi"
 )
 
@@ -43,7 +40,7 @@ var campusCmd = &cobra.Command{
 			log.Fatal().Msg("Failed to connect to database")
 		}
 
-		campus, err := duoapi.CampusAll()
+		campus, err := duoapi.CampusAll(cmd.Context())
 		if err != nil {
 			log.Fatal().Err(err).Msg("Failed to get duoapi response")
 		}
@@ -66,9 +63,9 @@ var campusCmd = &cobra.Command{
 				SetLanguageCode(c.Language.Identifier).
 				OnConflict().
 				DoNothing().
-				Exec(context.Background())
+				Exec(cmd.Context())
 			if err != nil {
-				if errors.Is(err, sql.ErrNoRows) {
+				if modelgen.IsNotFound(err) {
 					log.Debug().Msg("Campus " + c.Name + " already exists")
 					continue
 				}
