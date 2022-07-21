c9ae21032e826304a2c3ddef4f1e3b4bb5e96cea
Atomys <contact@atomys.fr>
2022-07-21 22:53:45 +0000

    feat: implement more actions on user context menu like intra profile and slack

--- web/ui/src/components/UserCard/DropDownMenu.tsx
+++ web/ui/src/components/UserCard/DropDownMenu.tsx
@@ -1,6 +1,6 @@
 import {
   useCreateFriendshipMutation,
-  useDeleteFriendshipMutation,
+  useDeleteFriendshipMutation
 } from '@graphql.d';
 import { Menu, Transition } from '@headlessui/react';
 import classNames from 'classnames';
@@ -8,7 +8,7 @@ import { Fragment } from 'react';
 import { DropdownMenuComponent } from './types';
 
 const DropdownMenu: DropdownMenuComponent = ({
-  userID,
+  user,
   isFriend = false,
   buttonAlwaysShow = false,
   refetchQueries = [],
@@ -38,23 +38,47 @@ const DropdownMenu: DropdownMenuComponent = ({
         >
           <Menu.Items
             className={classNames(
-              'absolute right-0 w-56 mt-2 origin-top-right bg-white dark:bg-slate-900 divide-y divide-gray-100 dark:divide-slate-800 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none',
+              'absolute right-0 w-56 mt-2 origin-top-right bg-white dark:bg-slate-900 divide-y divide-gray-100 dark:divide-slate-800/75 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none',
               buttonAlwaysShow ? 'visible' : 'invisible group-hover:visible'
             )}
           >
+            <div className="px-1 py-1">
+              <Menu.Item>
+                <a
+                  className="hover:bg-indigo-500 hover:text-white group flex rounded-md items-center w-full px-2 py-2 text-sm"
+                  href={`https://profile.intra.42.fr/users/${user.login}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fa-light fa-fw fa-address-card pr-2"></i>
+                  <span>Show Intranet profile</span>
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <a
+                  className="hover:bg-indigo-500 hover:text-white group flex rounded-md items-center w-full px-2 py-2 text-sm"
+                  href={`https://42born2code.slack.com/messages/@${user.login}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fa-brands fa-fw fa-slack pr-2"></i>
+                  <span>Contact on Slack</span>
+                </a>
+              </Menu.Item>
+            </div>
             <div className="px-1 py-1">
               {!isFriend && (
                 <Menu.Item>
                   <button
                     onClick={() => {
                       addFriendship({
-                        variables: { userID: userID },
+                        variables: { userID: user.id },
                         refetchQueries: refetchQueries,
                       });
                     }}
                     className="hover:bg-indigo-500 hover:text-white text-indigo-500 group flex rounded-md items-center w-full px-2 py-2 text-sm"
                   >
-                    <i className="fa-light fa-user-plus pr-2"></i>
+                    <i className="fa-light fa-fw fa-user-plus pr-2"></i>
                     <span>Add Friend</span>
                   </button>
                 </Menu.Item>
@@ -64,13 +88,13 @@ const DropdownMenu: DropdownMenuComponent = ({
                   <button
                     onClick={() => {
                       deleteFriendship({
-                        variables: { userID: userID },
+                        variables: { userID: user.id },
                         refetchQueries: refetchQueries,
                       });
                     }}
                     className="hover:bg-red-500 hover:text-white text-red-500 group flex rounded-md items-center w-full px-2 py-2 text-sm"
                   >
-                    <i className="fa-light fa-user-xmark pr-2"></i>
+                    <i className="fa-light fa-fw fa-user-xmark pr-2"></i>
                     <span>Remove Friend</span>
                   </button>
                 </Menu.Item>
