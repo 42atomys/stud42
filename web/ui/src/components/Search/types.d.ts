e430a968fe0d727aa472c89c1d8165c9bcf6c982
Atomys <contact@atomys.fr>
2022-07-23 13:11:19 +0000

    feat: search component reusability

---
+++ web/ui/src/components/Search/types.d.ts
@@ -0,0 +1,9 @@
+import { SearchUserQueryVariables, User } from '@graphql.d';
+
+export type SearchComponent = React.ComponentType<SearchProps>;
+
+export type SearchProps = {
+  action: (user: User) => Promise;
+  placeholder: string;
+  searchVariables?: Partial<SearchUserQueryVariables>;
+};
