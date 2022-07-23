7918c0ae6dcc33c2faaf1498fcece77ebe16e770
Atomys <contact@atomys.fr>
2022-07-23 10:23:16 +0000

    feat: implement cluster finder across all campus

--- web/ui/src/components/ClusterMap/ClusterContainer.tsx
+++ web/ui/src/components/ClusterMap/ClusterContainer.tsx
@@ -1,10 +1,22 @@
 import Loader from '@components/Loader';
 import useSidebar from '@components/Sidebar';
-import { UserPopup, PopupConsumer, PopupProvider } from '@components/UserPopup';
+import { PopupConsumer, PopupProvider, UserPopup } from '@components/UserPopup';
 import { useClusterViewQuery, User } from '@graphql.d';
 import { isFirstLoading } from '@lib/apollo';
+import { useRouter } from 'next/router';
+import { createContext, useEffect, useState } from 'react';
 import { ClusterSidebar } from '../../containers/clusters/ClusterSidebar';
-import { ClusterContainerComponent } from './types';
+import { ClusterContainerComponent, ClusterContextInterface } from './types';
+
+/**
+ * ClusterContext is a react context that holds the current cluster
+ * information. It is used to share the cluster information between the
+ * ClusterContainer and the ClusterMap components.
+ */
+export const ClusterContext = createContext<ClusterContextInterface>({
+  highlight: false,
+  hightlightVisibility: (_) => 'DIMMED',
+});
 
 /**
  * ClusterContainer component is used to display the cluster map and the cluster
@@ -34,16 +46,41 @@ export const ClusterContainer: ClusterContainerComponent = ({
   children,
 }) => {
   const { SidebarProvider, PageContainer, PageContent } = useSidebar();
+  const {
+    asPath,
+    query: { identifier: highlightedIdentifier },
+    replace,
+  } = useRouter();
+  const [highlight, setHighlight] = useState(false);
   const { data, networkStatus, error } = useClusterViewQuery({
     variables: { campusName: campus, identifierPrefix: cluster },
   });
+
+  useEffect(() => {
+    if (highlightedIdentifier) {
+      replace(asPath.split('?')[0], undefined, { shallow: true });
+      setHighlight(true);
+      const timer = setTimeout(() => setHighlight(false), 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [highlightedIdentifier]);
 
   return (
     <SidebarProvider>
       <PageContainer>
         <PopupProvider>
-          <>
-            <ClusterSidebar campus="paris" cluster={cluster as string} />
+          <ClusterContext.Provider
+            value={{
+              highlight: highlight,
+              hightlightVisibility: (identifier) =>
+                highlightedIdentifier === identifier ? 'HIGHLIGHT' : 'DIMMED',
+            }}
+          >
+            <ClusterSidebar
+              campus={campus.toLowerCase()}
+              cluster={cluster as string}
+            />
             <PageContent
               className={
                 'p-2 flex-1 flex justify-center min-h-screen items-center'
@@ -72,7 +109,7 @@ export const ClusterContainer: ClusterContainerComponent = ({
                 </PopupConsumer>
               )}
             </PageContent>
-          </>
+          </ClusterContext.Provider>
         </PopupProvider>
       </PageContainer>
     </SidebarProvider>
--- web/ui/src/pages/clusters/index.tsx
+++ web/ui/src/pages/clusters/index.tsx
@@ -4,7 +4,25 @@ type PageProps = {};
 
 const Home: NextPage<PageProps> = () => <></>;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { campus: campusQuery, identifier: identifierQuery } = query;
+  const campus = campusQuery?.toString().toLowerCase();
+  const identifier = identifierQuery?.toString().toLowerCase();
+
+  if (campus && identifier) {
+    const [_, cluster] = identifier.match(identifierValidator[campus]) || [];
+
+    if (Object.keys(identifierValidator).includes(campus) && cluster) {
+      return {
+        redirect: {
+          destination: `/clusters/${campus}/${cluster}?identifier=${identifier}`,
+          permanent: false,
+        },
+        props: {},
+      };
+    }
+  }
+
   return {
     redirect: {
       destination: '/clusters/paris/e1',
@@ -15,3 +33,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 export default Home;
+
+const identifierValidator: { [key: string]: RegExp } = {
+  paris: /(e(?:1|2|3)).{4,5}/i,
+  helsinki: /(c(?:1|2|3)).{4,5}/i,
+};
