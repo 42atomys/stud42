3eb8ebcf3d67a77ec4d29f4ead812961fc900dde
Atomys <contact@atomys.fr>
2022-07-23 10:18:59 +0000

    feat: implement redirect link on location badge

--- web/ui/src/components/Badge/LocationBadge.tsx
+++ web/ui/src/components/Badge/LocationBadge.tsx
@@ -1,6 +1,8 @@
+import ConditionalWrapper from '@components/ConditionalWrapper';
 import Emoji from '@components/Emoji';
 import { Location } from '@graphql.d';
 import classNames from 'classnames';
+import Link from 'next/link';
 import { NestedPartial } from 'types/utils';
 import { Badge } from './Badge';
 import { countryNameToEmoji } from './countryMap';
@@ -13,24 +15,37 @@ export const LocationBadge = ({
   const isConnected = location?.identifier ? true : false;
 
   return (
-    <Badge color={isConnected ? 'green' : 'gray'}>
-      <span
-        className={classNames(
-          'inline-flex rounded-full w-2 h-2',
-          isConnected ? 'bg-emerald-500' : 'bg-slate-500'
-        )}
-      ></span>
-      <span className="flex flex-row justify-center items-center text-sm mx-1">
-        {isConnected ? location?.identifier : 'Offline'}
-      </span>
-      {isConnected && (
-        <Emoji
-          emoji={countryNameToEmoji[location?.campus?.country || '']}
-          size={14}
-          title={location?.campus?.name}
-          className={classNames('mx-1', isConnected ? 'visible' : 'hidden')}
-        />
+    <ConditionalWrapper
+      // `? true : false` mysterious workaround to prevent ts error
+      condition={location?.campus?.name && location?.identifier ? true : false}
+      trueWrapper={(children) => (
+        <Link
+          href={`/clusters?campus=${location?.campus?.name?.toLowerCase()}&identifier=${location?.identifier
+            }`}
+        >
+          <a>{children}</a>
+        </Link>
       )}
-    </Badge>
+    >
+      <Badge color={isConnected ? 'green' : 'gray'}>
+        <span
+          className={classNames(
+            'inline-flex rounded-full w-2 h-2',
+            isConnected ? 'bg-emerald-500' : 'bg-slate-500'
+          )}
+        ></span>
+        <span className="flex flex-row justify-center items-center text-sm mx-1">
+          {isConnected ? location?.identifier : 'Offline'}
+        </span>
+        {isConnected && (
+          <Emoji
+            emoji={countryNameToEmoji[location?.campus?.country || '']}
+            size={14}
+            title={location?.campus?.name}
+            className={classNames('mx-1', isConnected ? 'visible' : 'hidden')}
+          />
+        )}
+      </Badge>
+    </ConditionalWrapper>
   );
 };
