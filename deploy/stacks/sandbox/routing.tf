4453e64dcaffdcd7b0d89d1b9b3a3bc16fded430
42Atomys <contact@atomys.fr>
2022-11-17 15:57:27 +0100

    feat: deploy the sandbox environment for s42 devs (#255)
    **Describe the pull request**
    
    With the new IaC, the sandbox is not automated anymore. 
    
    **Breaking changes ?**
    no
    
    Signed-off-by: 42Atomys <contact@atomys.fr>

---
+++ deploy/stacks/sandbox/routing.tf
@@ -0,0 +1,50 @@
+module "istio" {
+  source = "../../modules/istio"
+
+  virtual_services = {
+    "${local.reversedRootDomain}" = {
+      namespace = var.namespace
+      hosts     = [var.rootDomain]
+      gateways  = [local.reversedRootDomain]
+      http = [
+        {
+          name = "jwtks-service-public-sign"
+          route = [{
+            destination = {
+              host = "jwtks-service.${var.namespace}.svc.cluster.local"
+              port = {
+                number = 5000
+              }
+            }
+          }]
+        },
+        {
+          name = "jwtks-service"
+          match = [
+            {
+              uri = {
+                prefix = "/.well-known/jwks"
+              }
+              method = {
+                exact = "GET"
+              }
+            }
+          ]
+          rewrite = {
+            uri = "/jwks"
+          }
+          route = [
+            {
+              destination = {
+                host = "jwtks-service.${var.namespace}.svc.cluster.local"
+                port = {
+                  number = 5500
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
