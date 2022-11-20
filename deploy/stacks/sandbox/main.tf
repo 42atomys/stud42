23c2172a2f49aa724457bda1524826aaab9bb8d0
42Atomys <contact@atomys.fr>
2022-11-20 20:23:06 +0100

    fix: sandbox  deployment crashes and discord is not updated anymore (#258)
    **Describe the pull request**
    
    The sandbox stacks don't have the s3 backend configured and discord is
    not updated on release. This PR will fixes this issues
    
    **Checklist**
    
    - [x] I have linked the relative issue to this pull request
    - [x] I have made the modifications or added tests related to my PR
    - [x] I have added/updated the documentation for my RP
    - [x] I put my PR in Ready for Review only when all the checklist is
    checked
    
    **Breaking changes ?**
    no

--- deploy/stacks/sandbox/main.tf
+++ deploy/stacks/sandbox/main.tf
@@ -1,4 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket                      = "s42-terraform-state"
+    key                         = "sandbox.tfstate"
+    endpoint                    = "https://s3.gra.io.cloud.ovh.net/"
+    region                      = "gra"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+
+
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
