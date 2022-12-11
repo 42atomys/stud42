b7c546f2bc807fe6a050a821e91e43457978a6bb
42Atomys <gregory.dalmar@hivebrite.com>
2022-12-11 01:57:35 +0100

    fix: grpc / protobuf install on arm64

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -15,3 +15,15 @@ fi
 echo "Downloading protoc... $URL"
 wget -qO /tmp/protoc.zip $URL
 unzip -uo /tmp/protoc -d /usr/local bin/protoc
+
+# Special installation of grpc-tools due to missing precompiled binaries 
+# on linux ARM64
+# See https://github.com/grpc/grpc-node/issues/1405
+if [[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64"  ]]; then
+  echo "Manual install of grpc-tools on arm64."
+
+  yarn add --cwd web/ui grpc-tools --ignore-scripts
+  pushd web/ui/node_modules/grpc-tools || exit
+  node_modules/.bin/node-pre-gyp install --target_arch=x64
+  popd || return
+fi
