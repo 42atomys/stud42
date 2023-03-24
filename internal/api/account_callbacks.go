dfd95977b353a8b9bb07f731d39092abae92dd5f
c3b5aw <pro@c3b5aw.dev>
2023-03-23 18:41:36 -0700

    fix(api): remove auto star & auto follow

--- internal/api/account_callbacks.go
+++ internal/api/account_callbacks.go
@@ -9,16 +9,7 @@ import (
 	"atomys.codes/stud42/internal/models/generated/user"
 	"atomys.codes/stud42/pkg/utils"
 	"github.com/getsentry/sentry-go"
-	"github.com/google/go-github/v47/github"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/oauth2"
-)
-
-const (
-	// githubCreator is the GitHub username of the creator of the repository.
-	githubCreator = "42Atomys"
-	// githubRepository is the GitHub repository name.
-	githubRepository = "stud42"
 )
 
 // accountLinkCallback is the callback for all accounts. This is used to define
@@ -74,30 +65,7 @@ func discordLinkCallback(ctx context.Context, db *modelgen.Client, account *mode
 // linked to an user. It will follow the creator of the repository and star
 // the repository.
 func githubLinkCallback(ctx context.Context, db *modelgen.Client, account *modelgen.Account) {
-	client := github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: account.AccessToken},
-	)))
-
-	// Star the repository if it's not already starred
 	var err error
-	var ok bool
-	if ok, _, err = client.Activity.IsStarred(ctx, githubCreator, githubRepository); !ok {
-		_, err = client.Activity.Star(ctx, githubCreator, githubRepository)
-	}
-	if err != nil {
-		log.Error().Err(err).Msg("failed to star the repository")
-		sentry.CaptureException(err)
-	}
-
-	// Follow th creator if it's not already followed
-	if ok, _, err = client.Users.IsFollowing(ctx, "", githubCreator); !ok {
-		_, err = client.Users.Follow(ctx, githubCreator)
-	}
-	if err != nil {
-		log.Error().Err(err).Msg("failed to follow the creator")
-		sentry.CaptureException(err)
-	}
-
 	// Give beta access to the user
 	u, err := db.User.Query().Where(user.ID(account.UserID)).First(ctx)
 	if err != nil {
