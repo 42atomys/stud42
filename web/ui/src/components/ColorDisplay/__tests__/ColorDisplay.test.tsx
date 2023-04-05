584c756e3dcd1245d2e9cf994e6914a2e4a85618
42Atomys <contact@atomys.fr>
2023-04-05 12:10:42 +0200

    test: add jest types and color display tests suite

---
+++ web/ui/src/components/ColorDisplay/__tests__/ColorDisplay.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { ColorDisplay } from '../ColorDisplay';
+
+describe('ColorDisplay', () => {
+  it('renders the div with the given background color', () => {
+    const color = '#ff0000';
+    const { container } = render(<ColorDisplay color={color} />);
+    const divElement = container.firstChild as HTMLDivElement;
+    expect(divElement).toHaveStyle({
+      backgroundColor: color,
+      outlineColor: color,
+    })
+  });
+
+  it('renders the div with transparent background color when color prop is not given', () => {
+    const { container } = render(<ColorDisplay color={null} />);
+    const divElement = container.firstChild as HTMLDivElement;
+    expect(divElement).toHaveStyle({
+      backgroundColor: 'transparent',
+      outlineColor: '',
+    })
+  });
+
+  it('throws an error when color prop is not a valid color', () => {
+    const { container } = render(<ColorDisplay color={'invalid'} />);
+    const divElement = container.firstChild as HTMLDivElement;
+    expect(divElement.style.backgroundColor).toBe('');
+  });
+});
