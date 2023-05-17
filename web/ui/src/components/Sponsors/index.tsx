f697dfc5c57a269fa6a1522b114b6f1b201a2ac1
Atomys <contact@atomys.fr>
2023-05-17 20:59:29 +0200

    featL add some sponsors stuff

---
+++ web/ui/src/components/Sponsors/index.tsx
@@ -0,0 +1,33 @@
+import { Tooltip } from '@components/Tooltip';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { PropsWithClassName } from 'types/globals';
+
+export const SponsorIcon: React.FC<PropsWithClassName> = ({ className }) => (
+  <i
+    className={classNames(
+      className,
+      'fa-solid fa-user-astronaut text-fuchsia-400 dark:text-fuchsia-600'
+    )}
+  ></i>
+);
+
+export const SponsorHint: React.FC<PropsWithClassName> = ({ className }) => (
+  <Tooltip
+    text="Sponsors Only"
+    subText="This feature is only available to sponsors."
+    color="fuchsia"
+    direction="top"
+    size="sm"
+    showArrow
+  >
+    <Link
+      href="https://github.com/sponsors/42Atomys"
+      target="_blank"
+      rel="noreferrer"
+      className="text-white font-bold hover:underline"
+    >
+      <SponsorIcon className={className} />
+    </Link>
+  </Tooltip>
+);
