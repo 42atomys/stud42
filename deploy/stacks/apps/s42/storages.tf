16cab4152204ebc4106fbdc6958043d28421d81f
42Atomys <contact@atomys.fr>
2023-05-20 14:01:26 +0200

    chore: add dragonfly stack for production

--- deploy/stacks/apps/s42/storages.tf
+++ deploy/stacks/apps/s42/storages.tf
@@ -3,6 +3,13 @@ locals {
   rabbitmqClusterReference = {
     name = local.rabbitmqClusterName
   }
+
+  dragonflyProbe = {
+    httpGet = {
+      path = "/"
+      port = "dragonfly"
+    }
+  }
 }
 
 resource "kubernetes_manifest" "rabbitmq" {
@@ -215,6 +222,99 @@ module "postgres" {
     data = {
       accessModes      = ["ReadWriteOnce"]
       storage          = var.namespace == "production" ? "10Gi" : "1Gi"
+      storageClassName = "csi-cinder-high-speed"
+    }
+  } : {}
+}
+
+resource "random_password" "dragonfly" {
+  length  = 64
+  special = true
+}
+
+module "dragonfly" {
+  source = "../../../modules/service"
+  kind   = "StatefulSet"
+
+  appName         = "dragonfly"
+  appVersion      = "v1.3.0"
+  name            = "dragonfly"
+  namespace       = var.namespace
+  image           = "docker.dragonflydb.io/dragonflydb/dragonfly:v1.3.0"
+  imagePullPolicy = "IfNotPresent"
+
+  nodeSelector = local.nodepoolSelector["storages"]
+
+  revisionHistoryLimit = 1
+  replicas             = 1
+  maxUnavailable       = 0
+
+  prometheus = {
+    enabled = true
+    port    = 6379
+  }
+
+  livenessProbe  = local.dragonflyProbe
+  readinessProbe = local.dragonflyProbe
+
+  resources = {
+    requests = {
+      cpu    = "100m"
+      memory = "128Mi"
+    }
+
+    limits = {
+      memory = "256Mi"
+    }
+  }
+
+  env = {}
+
+  envFromSecret = {
+    DFLY_PASSWORD = {
+      key  = "DFLY_PASSWORD"
+      name = "dragonfly-credentials"
+    }
+  }
+
+  ports = {
+    dragonfly = {
+      containerPort = 6379
+      istioProtocol = "tcp"
+    }
+  }
+
+  volumeMounts = [
+    {
+      mountPath  = "/data"
+      volumeName = "data"
+    }
+  ]
+
+  volumesFromPVC = var.hasPersistentStorage ? {
+    data = {
+      claimName = "dragonfly-data"
+      readOnly  = false
+    }
+  } : {}
+
+  volumesFromEmptyDir = !var.hasPersistentStorage ? {
+    data = {}
+  } : {}
+
+  secrets = {
+    credentials = {
+      data = {
+        "DFLY_PASSWORD"         = random_password.dragonfly.result
+        "DFLY_PASSWORD_ENCODED" = urlencode(random_password.dragonfly.result)
+      }
+    }
+  }
+
+  persistentVolumeClaims = var.hasPersistentStorage ? {
+    data = {
+      accessModes      = ["ReadWriteMany"]
+      storage          = var.namespace == "production" ? "5Gi" : "1Gi"
       storageClassName = "csi-cinder-high-speed"
     }
   } : {}
