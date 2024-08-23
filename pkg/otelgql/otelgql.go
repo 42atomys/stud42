8c40b25575dc5e5671c421b70ddfc65b67606543
Atomys <contact@atomys.fr>
2024-08-23 15:07:52 +0200

    Update otelgql.go
    Signed-off-by: Atomys <contact@atomys.fr>

--- pkg/otelgql/otelgql.go
+++ pkg/otelgql/otelgql.go
@@ -84,7 +84,7 @@ func (a Tracer) InterceptResponse(ctx context.Context, next graphql.ResponseHand
 	resp := next(ctx)
 	if resp != nil && len(resp.Errors) > 0 {
 		span.SetStatus(codes.Error, resp.Errors.Error())
-		span.RecordError(resp.Errors.Error())
+		span.RecordError(errors.New(resp.Errors.Error()))
 		span.SetAttributes(ResolverErrors(resp.Errors)...)
 	}
 
@@ -115,7 +115,7 @@ func (a Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (inte
 	errList := graphql.GetFieldErrors(ctx, fc)
 	if len(errList) != 0 {
 		span.SetStatus(codes.Error, errList.Error())
-		span.RecordError(errList.Error())
+		span.RecordError(errList)
 		span.SetAttributes(ResolverErrors(errList)...)
 	}
 
